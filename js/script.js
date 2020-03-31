ce40e3ba68d83a5258574a59970dea7e3d1069f0
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-03-31 15:39:26 +0300

    add 4 lesson

--- js/script.js
+++ js/script.js
@@ -1,55 +1,17 @@
 'use strict';
 
-/*First Task*/
-let lang = 'ru';
+/*Lesson 04*/
 
-/*IF*/
-if (lang === 'ru') {
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресеье');
-} else if (lang === 'en') {
-    console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-} else {
-    console.log('Нет совпадений');
-}
-
-
-/*SWITCH CASE*/
-switch (lang) {
-    case 'ru':
-        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресеье');
-        break;
-        case "en":
-            console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-            break;
-    default:
-        console.log('Нет совпадений');
-}
-
-/*MULTIDIMENSIONAL ARRAY*/
-lang = prompt("введите ru или en");
-let arr = {
-    'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
-    'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+const foo = function(a) {
+    if (typeof a !== 'string') {
+        alert('В качестве аргумента передана не строка');
+    } else if (a.length > 30) {
+        let str = a.slice(0, 29) + '...';
+        console.log(str.trim())
+    }
 };
-alert(arr[lang]);
 
-
-
-
-
-
-/*Second Task*/
-let namePerson = 'Дженнифер Энистон';
-
-let result = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
-console.log(result);
-
-
-
-
-
-
-
+foo(' Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки         ');
 
 
 
