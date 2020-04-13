25c805313c0259e4bc54c538463f460eaa63b79a
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-04-13 15:18:46 +0300

    lesson 12

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 'use strict';
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    toDay = new Date().getDay() - 1;
+let colorText = document.getElementById('color'),
+    buttonChange = document.getElementById('change');
 
-week.forEach(function(item, i) {
-    if (i === toDay) {
-        document.write('<b>' + week[i] + '</b></br>');
-    } else if (week[i] === 'Суббота' || week[i] === 'Воскресенье') {
-        document.write('<i>' + week[i] + '</i></br>');
-    } else {
-        document.write(week[i] + '</br>');
-    }
+
+buttonChange.addEventListener('click', function() {
+        let r = Math.floor(Math.random() * (256)),
+            g = Math.floor(Math.random() * (256)),
+            b = Math.floor(Math.random() * (256)),
+            colorChange = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+    document.body.style.backgroundColor = colorChange;
+    colorText.textContent = colorChange;
 });
-
 
 
 
